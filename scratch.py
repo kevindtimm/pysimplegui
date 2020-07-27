4929237c9296ecae2b2192b465719603f2aac754
kevindtimm <kevindtimm@gmail.com>
2020-07-26 21:00:42 -0500

    updates for titles, etc

--- scratch.py
+++ scratch.py
@@ -3,20 +3,22 @@ import PySimpleGUI as sg
 
 # layout definition
 layout = [
-  [sg.Text('My Layout')],
-  [sg.Input(key='-INPUT-')],
-  [sg.Button('OK', bind_return_key=True), sg.Button('Cancel')]
+    [sg.Text('My Layout')],
+    [sg.Input(key='-INPUT-')],
+    [sg.Button('OK', bind_return_key=True), sg.Button('Cancel')]
 ]
 
 # create the window
-window = sg.Window('Design Pattern 3 - Persistent Window', layout)
+window = sg.Window(title='Design Pattern 3 - Persistent Window', layout=layout)
 
 # event loop
 while True:
-  event, values = window.read()
-  print (event, values)
-  if event in (None, 'Cancel'):
-    break
+    event, values = window.read()
+    print (event, values)
+    if event in (None, 'Cancel'):
+        break
+    if event == 'OK':
+        sg.Popup(f'values', values['-INPUT-'])
 
 # close the window
 window.close()
